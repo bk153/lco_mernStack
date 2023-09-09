bed046784ad9bfb204fd226475a6f545f8b7458f
BhawaniK <kundala.bhawani@gmail.com>
2023-09-10 00:19:26 +0530

    feature_Added OrderSchema

---
+++ models/order.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const ProductInCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [ProductInCartSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    updated: Date,
+    user: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+const ProductInCart = mongoose.model("Product", ProductInCartSchema);
+
+module.exports = { Order, ProductInCart };
