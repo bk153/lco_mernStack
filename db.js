14db4cca6821d60f6d3e75aac33837b242f8b61f
BhawaniK <bk.sambhaw@gmail.com>
2023-09-11 17:47:01 +0530

    feature_added DbConnection

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('DB CONNECTED--');
+})
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
+const db = require('./db')
+
 const app = express()
-const PORT = 7000
+const PORT = process.env.PORT | 7000
 
 app.get('/', (req, res) => {
   res.send('Welcome!..')
